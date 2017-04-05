15fc7da486094a8ae53c69321cef4d60dfbf2294
Alexandar Kotevski <alexandarkotevski@alexandars-mbp.home>
2017-04-05 12:24:19 +1000

    update

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang=“en-AU”>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+
+<h1 align="center"><img src="media/img/logo.png" alt="Logo" height="180" width="260"></h1>
+
+<p align="center">Username:<br/>Password:<br/>Log in<br/></p>
+
+</body>
+</html>
